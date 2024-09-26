1c23d91626ba4337e861ddf08f96b17fb79d8b6a
Pellumi <pellumii67@gmail.com>
2024-09-26 11:00:23 +0100

    finally deployed on render

--- index.js
+++ index.js
@@ -56,5 +56,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
